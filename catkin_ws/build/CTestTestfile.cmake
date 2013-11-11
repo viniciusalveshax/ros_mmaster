dbc53855f9ed6ef26053db5a8e906f2af84ff4f4
Vinicius Alves Hax <viniciushax@gmail.com>
2013-11-11 18:50:21 -0200

    Initial commit to ROS MMaster Package

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vinicius/Dropbox/arv/projetos/mestradocomp/Dissertacao/mmaster_hydro/catkin_ws/src
+# Build directory: /home/vinicius/Dropbox/arv/projetos/mestradocomp/Dissertacao/mmaster_hydro/catkin_ws/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(ros_mmaster)
