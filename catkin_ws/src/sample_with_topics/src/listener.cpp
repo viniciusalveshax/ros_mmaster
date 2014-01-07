1e976e26cde47b9c66eb0865ac6f9ac60a1cc214
Vinícius Alves Hax <viniciushax@gmail.com>
2014-01-06 23:06:49 -0200

    Minor change in code formatation

--- catkin_ws/src/sample_with_topics/src/listener.cpp
+++ catkin_ws/src/sample_with_topics/src/listener.cpp
@@ -41,6 +41,7 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
+		
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line. For programmatic
