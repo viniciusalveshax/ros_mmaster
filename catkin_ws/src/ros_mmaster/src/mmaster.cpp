e7fb8050f2cf310cb03fecdcdc6e61134e1916cc
Vinícius Alves Hax <viniciushax@gmail.com>
2014-01-11 22:32:34 -0200

    Removing old pieces of code not used. Encapsuling XmlRpcServer in a thread. Changing the main loop to finish XmlRpcServer when input is "exit".

--- catkin_ws/src/ros_mmaster/src/mmaster.cpp
+++ catkin_ws/src/ros_mmaster/src/mmaster.cpp
@@ -1,4 +1,4 @@
-#define DEBUG
+//#define DEBUG
 
 #include "ros/ros.h"
 #include <string>
@@ -7,6 +7,7 @@
 #include "XmlRpc.h"
 #include "ros/xmlrpc_manager.h"
 #include "../include/ros_mmaster/mmasternode.h"
+#include <boost/thread.hpp>
 
 using namespace std;
 using namespace XmlRpc;
@@ -14,83 +15,6 @@ using namespace XmlRpc;
 MMasterNode mmaster;
 XmlRpcServer s;
 
-/*  
-
-  if (message == "quit")
-  // TODO
-  // if a mmaster node has quit then ...
-  // update mmaster next and prev address
-
-  if (message == "solve")
-  // TODO
-  // consult internal table
-    if (haveResult)
-      // TODO
-      // send a reply with result
-    else 
-      {
-      // TODO
-      // consult other masters
-      if (should_i_know)
-        //TODO
-        // i am responsible by the result but a i dont have info
-        // send reply to centralized Master node        
-        // get info
-      else
-        // forward requisition to another mmaster node
-        // get info
-      // now i have the necessary info
-      // send a reply with result
-      }
-*/
-
-// No arguments, result is "Hello".
-class Hello : public XmlRpcServerMethod
-{
-public:
-  Hello(XmlRpcServer* s) : XmlRpcServerMethod("Hello", s) {}
-
-  void execute(XmlRpcValue& params, XmlRpcValue& result)
-  {
-    result = "Hello";
-  }
-
-  std::string help() { return std::string("Say hello"); }
-
-} hello(&s);    // This constructor registers the method with the server
-
-
-// One argument is passed, result is "Hello, " + arg.
-class HelloName : public XmlRpcServerMethod
-{
-public:
-  HelloName(XmlRpcServer* s) : XmlRpcServerMethod("HelloName", s) {}
-
-  void execute(XmlRpcValue& params, XmlRpcValue& result)
-  {
-    std::string resultString = "Hello, ";
-    resultString += std::string(params[0]);
-    result = resultString;
-  }
-} helloName(&s);
-
-
-// Original sample code by XmlRPC++
-// http://xmlrpcpp.sourceforge.net/faq.html
-class Sum : public XmlRpcServerMethod
-{
-public:
-  Sum(XmlRpcServer* s) : XmlRpcServerMethod("Sum", s) {}
-
-  void execute(XmlRpcValue& params, XmlRpcValue& result)
-  {
-    int nArgs = params.size();
-    double sum = 0.0;
-    for (int i=0; i<nArgs; ++i)
-      sum += double(params[i]);
-    result = sum;
-  }
-} sum(&s);
 
 class registerSubscriber : public XmlRpcServerMethod
 {
@@ -157,10 +81,15 @@ public:
 } set_previous(&s);
 */
 
+void xml_rpc_server_listen(void) {
+  s.bindAndListen(mmaster.port());
+  s.work(-1.0);
+}
+
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "mmaster");
+  ros::init(argc, argv, "mmaster", ros::init_options::AnonymousName);
 
   if (!mmaster.readAddressMaster())
   {
@@ -169,6 +98,8 @@ int main(int argc, char **argv)
   }
 
   mmaster.setHostname("localhost");
+  
+  srand(time(NULL));
   mmaster.setPort((rand() % 64512) + 1024);    // choose a random a port between 1024 and 65536																																									// ports below 1024 requires root privileges and
  																																												// max port number is 65536
   ros::NodeHandle node_handle;
@@ -194,8 +125,7 @@ int main(int argc, char **argv)
   
 //  XmlRpc::setVerbosity(3);
 
-  s.bindAndListen(mmaster.port());
-  s.work(-1.0);
+  boost::thread xml_rpc_server_thread(xml_rpc_server_listen);
 
   #ifdef DEBUG
   cout << "main: Vou entrar no laço de leitura do teclado" << endl;
@@ -217,6 +147,9 @@ int main(int argc, char **argv)
       }
     }
 
+  // Shutdown XML RPC Server
+  s.shutdown();
+
   // Get updated list from parameter server
   node_handle.getParam("/mmaster_addresses", mmaster_addresses);
 
@@ -231,6 +164,7 @@ int main(int argc, char **argv)
     node_handle.setParam("/mmaster_addresses", mmaster_addresses);
     }
   else
+    // I am the last server, remove the param
     node_handle.deleteParam("/mmaster_addresses");
 
   return 0;
